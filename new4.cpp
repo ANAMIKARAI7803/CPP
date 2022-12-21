dfca4bde8f5a34bcb0c7df8040058934b5c29592
Anamika Rai <104133325+ANAMIKARAI7803@users.noreply.github.com>
2022-12-21 14:28:31 +0530

    Add files via upload

---
+++ new4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    if(n%2==0){
+        cout<<"Even";
+
+    }
+    else {
+        cout<<"odd";
+
+    }
+    return 0;
+}
---
+++ new5.cpp
@@ -0,0 +1,13 @@
+// for loop
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int sum=0;
+    for (int counter=1;counter<=n;counter++){
+        sum=sum+counter;
+    }
+    return 0;
+}
